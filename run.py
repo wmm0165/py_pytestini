255f6428f75b853634ba1f125a77a8742166ac61
wangmengmeng <wmm_0165@163.com>
2020-03-18 18:16:51 +0800

    标记测试用例

---
+++ run.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/3/18 18:11
+# @Author : wangmengmeng
+import pytest
+
+
+if __name__ == '__main__':
+    pytest.main(["-m","P0"])
--- testcase/test_01.py
+++ testcase/test_01.py
@@ -2,5 +2,14 @@
 # @Time : 2019/11/2 19:56
 # @Author : wangmengmeng
 
+import pytest
+
+@pytest.mark.P0
 def test_01():
+    print("1")
     assert 1 == 1
+
+
+def test_02():
+    assert 1 == 1
+
